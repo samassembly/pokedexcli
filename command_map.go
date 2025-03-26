6f9dbfd9e325b40094e5d54caa92f3e5846a147f
SamPCS <svogel@pcsknox.com>
2025-03-26 15:54:51 -0400

    Fixed pokeapi location, Removed buggy code from package refactor

--- command_map.go
+++ command_map.go
@@ -1,47 +1,40 @@
 package main
 
 import (
-	"fmt"
 	"errors"
+	"fmt"
 )
 
-//get the next 20 locations from pokeapi
 func commandMapf(cfg *config) error {
 	locationsResp, err := cfg.pokeapiClient.ListLocations(cfg.nextLocationsURL)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
-	//updates the structs next and previous location calls
 	cfg.nextLocationsURL = locationsResp.Next
 	cfg.prevLocationsURL = locationsResp.Previous
 
-	//print locations
 	for _, loc := range locationsResp.Results {
 		fmt.Println(loc.Name)
 	}
 	return nil
 }
 
-//get the previous 20 locations from pokeapi
 func commandMapb(cfg *config) error {
-	//check if first
 	if cfg.prevLocationsURL == nil {
-		return errors.New("This is the first page of locations")
+		return errors.New("you're on the first page")
 	}
-	//get batch of locations
+
 	locationResp, err := cfg.pokeapiClient.ListLocations(cfg.prevLocationsURL)
 	if err != nil {
 		return err
 	}
-	//updates the structs next and previous location calls
-	cfg.nextLocationsURL = locationsResp.Next
-	cfg.prevLocationsURL = locationsResp.previous
 
-	//print locations
+	cfg.nextLocationsURL = locationResp.Next
+	cfg.prevLocationsURL = locationResp.Previous
+
 	for _, loc := range locationResp.Results {
 		fmt.Println(loc.Name)
 	}
 	return nil
-
 }
--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"time"
 
-	"github.com/bootdotdev/pokedexcli/internal/pokeapi"
+	"github.com/samassembly/pokedexcli/internal/pokeapi"
 )
 
 func main() {
